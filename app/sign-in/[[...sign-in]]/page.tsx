e7cc140ae5df8362dae7ddce76a37eab38e24a55
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-21 10:06:20 +0530

    Update sign-in and sign-up pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn />;
+    return <div className="flex items-center justify-center flex-col gap-10">
+        <h1 className="text-4xl font-bold mt-20 ">This is the SignIn Page</h1>
+        <SignIn />;
+    </div>
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignUp />;
+    return <div className="flex items-center justify-center flex-col gap-10">
+        <h1 className="text-4xl font-bold mt-20 ">This is the SignUp Page</h1>
+        <SignUp />;
+    </div>
 }
